1835c0df2051893969da64f9a0bb8df7f0866ca2
Niklas Tluk <niklas.tluk@study.hs-duesseldorf.de>
2025-10-15 16:21:53 +0200

    Import and Place 3D Assets during Runtime

---
+++ Assets/LoadObject3D.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class LoadObject3D : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject object3D = new GameObject("GameObject3", typeof(Rigidbody), typeof(BoxCollider));
+        object3D.transform.position = new Vector3(5, 10, 0);
+        var gltf = object3D.AddComponent<GLTFast.GltfAsset>();
+        gltf.Url = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF/Duck.gltf";
+
+        GameObject object3D2 = new GameObject("GameObject3", typeof(Rigidbody), typeof(BoxCollider));
+        object3D2.transform.position = new Vector3(0, 10, 0);
+        object3D2.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+        var gltf2 = object3D2.AddComponent<GLTFast.GltfAsset>();
+        gltf2.Url = "file://D:\\Comfy\\ComfyUI_h2_1\\ComfyUI\\output\\Hy21_Mesh.glb";
+    }
+}
--- Assets/Scripts/TextToImage.cs
+++ Assets/Scripts/TextToImage.cs
@@ -11,7 +11,6 @@ public class TextToImage : MonoBehaviour
 {
     public Process process;
     public StreamWriter streamWriter;
-    public MovePicture movePicture;
     private Thread thread;
 
     private List<string> liLines = new List<string>();
@@ -47,21 +46,10 @@ public class TextToImage : MonoBehaviour
     {
         //streamWriter.WriteLine($"cd D:/Projects/AAGEPlus/repo/Python");
         streamWriter.WriteLine($"cd C:/Projekte/AIArtExtendedPlus/Python");
-        if (movePicture.simpsons)
-        {
-            streamWriter.WriteLine($"python simpsons_workflow.py");
-            UnityEngine.Debug.Log("Writing: " + $"queueing simpsons");
-        }
-        else if (movePicture.mosaic)
-        {
-            streamWriter.WriteLine($"python mosaic_workflow.py");
-            UnityEngine.Debug.Log("Writing: " + $"queueing mosaic");
-        }
-        else
-        {
-            streamWriter.WriteLine($"python txt2img_workflowUDO.py");
-            UnityEngine.Debug.Log("Writing: " + $"queueing txt2img");
-        }
+        
+        streamWriter.WriteLine($"python txt2img_workflowUDO.py");
+        UnityEngine.Debug.Log("Writing: " + $"queueing txt2img");
+        
             
     }
 }
