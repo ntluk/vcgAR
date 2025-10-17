df36d8f3d00efdd553e8418d6f4acbdec9b1633d
Niklas Tluk <niklas.tluk@study.hs-duesseldorf.de>
2025-10-17 17:15:35 +0200

    Feature Test Scene  + Segmentation Workflow Integration

--- Assets/LoadObject3D.cs
+++ Assets/LoadObject3D.cs
@@ -10,8 +10,8 @@ public class LoadObject3D : MonoBehaviour
         gltf.Url = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF/Duck.gltf";
 
         GameObject object3D2 = new GameObject("GameObject3", typeof(Rigidbody), typeof(BoxCollider));
-        object3D2.transform.position = new Vector3(0, 10, 0);
-        object3D2.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+        object3D2.transform.position = new Vector3(4.888301f, 1f, 2f);
+        object3D2.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
         var gltf2 = object3D2.AddComponent<GLTFast.GltfAsset>();
         gltf2.Url = "file://D:\\Comfy\\ComfyUI_h2_1\\ComfyUI\\output\\Hy21_Mesh.glb";
     }
---
+++ Python/segmentation_workflow.py
@@ -0,0 +1,22 @@
+import json
+from urllib import request, parse
+import random
+
+
+def queue_prompt(prompt_workflow):
+    p = {"prompt": prompt_workflow}
+    data = json.dumps(p).encode('utf-8')
+    req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
+    request.urlopen(req)    
+    
+# load the workflow from file, assign it to variable named prompt_workflow
+prompt_workflow = json.load(open('D:/Projects/VisualContentGenerationAR/vcgAR/Python/SegmentSelection.json', 'r', encoding='utf-8'))
+
+set_coords = prompt_workflow["38"]
+
+
+set_coords["inputs"]["points_store"] = "{\"positive\":[{\"x\":1260.0,\"y\":600.0}],\"negative\":[{\"x\":0,\"y\":0}]}"
+set_coords["inputs"]["coordinates"] = "[{\"x\":1260.0,\"y\":600.0}]"
+
+
+queue_prompt(prompt_workflow)
